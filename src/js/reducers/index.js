d57a8a7843ab2eb42364950cf3cdc5e22d8e861c
Alberto Marín <alberto.marin@converfit.com>
2019-03-18 12:14:50 +0100

    Failing at posts

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 const initialState = {
   articles: []
 };
@@ -6,6 +6,11 @@ function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
+    });
+  }
+  if (action.type === "DATA_LOADED") {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
     });
   }
   return state;
